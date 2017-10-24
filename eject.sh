bae2c261b32092ce98ccfa89ba0b18087cbc4123
Ilya Antipenko <aivus@users.noreply.github.com>
2017-10-25 00:26:21 +0300

    Create eject.sh

---
+++ eject.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# This script allows to eject USB disk from the DSM
+# Usage:
+# ./eject.sh sdq
+# Tested on DSM 6.1.3-15152 Update 8
+
+# disk="sdq";
+disk=$1;
+
+if [ ! -e  "/dev/${disk}" ] ; then
+    echo "Disk /dev/${disk} not found. Exiting.";
+    exit 1;
+fi
+
+# Sleep to be sure that disk mounted properly after boot-up
+# sleep 60;
+
+# Sync writes
+sync;
+# Unmount sdq disks (all partitions)
+/usr/syno/bin/synousbdisk -umount ${disk};
+# Safe sleep
+sleep 5;
+# Remove disk from the usbtab file to remove it from the DSM
+sed -i "/${disk}/d" /tmp/usbtab;
