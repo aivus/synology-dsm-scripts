2cc3600821cc07d7dddae7089b983732e9564519
Illia Antypenko <ilya@antipenko.pp.ua>
2022-10-24 01:58:50 +0300

    Update reconnect-vpn.sh

--- reconnect-vpn.sh
+++ reconnect-vpn.sh
@@ -4,7 +4,7 @@
 # Stick to the commit hash for security reason
 
 if [ ! -f /root/reconnect-vpn.sh ]; then
-    curl https://github.com/ianharrier/synology-scripts/blob/05b3c740caccd1ff0a6e9693e2636c36951a405d/reconnect-vpn.sh --output /root/reconnect-vpn.sh
+    curl https://raw.githubusercontent.com/ianharrier/synology-scripts/05b3c740caccd1ff0a6e9693e2636c36951a405d/reconnect-vpn.sh --output /root/reconnect-vpn.sh
     chmod +x reconnect-vpn.sh
 fi
 
