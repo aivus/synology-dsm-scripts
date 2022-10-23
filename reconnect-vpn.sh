5092068266c65b625dc7be6de6fe3b8b260fdfc9
Illia Antypenko <ilya@antipenko.pp.ua>
2022-10-24 01:51:39 +0300

    Create reconnect-vpn.sh

---
+++ reconnect-vpn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download external script if it doesn't exist
+# Stick to the commit hash for security reason
+
+if [ ! -f /root/reconnect-vpn.sh ]; then
+    curl https://github.com/ianharrier/synology-scripts/blob/05b3c740caccd1ff0a6e9693e2636c36951a405d/reconnect-vpn.sh --output /root/reconnect-vpn.sh
+    chmod +x reconnect-vpn.sh
+fi
+
+/root/reconnect-vpn.sh
